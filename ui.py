4bf1e7be00bf7c5b0548b7d9fba066be948b80d9
Karn <karnssk123@gmail.com>
2023-09-13 21:59:34 +0700

    Updated StringVar

--- ui.py
+++ ui.py
@@ -2,115 +2,127 @@ from tkinter import *
 
 root = Tk()
 root.title("Sudoku James Asked")
-varrowcol = [[],[],[],[],[],[],[],[],[]]
+rowcol = [[],[],[],[],[],[],[],[],[]]
+# varrowcol =[[],[],[],[],[],[],[],[],[]]
 for i in range(9):
     for j in range(9):
-        varrowcol[i].append(0)
+        rowcol[i].append(0)
+        # varrowcol[i].append(IntVar)
 Label( text="Sudoku James Asked", font=24).pack()
 frm1 = Frame(root)
+vr1c1, vr1c2, vr1c3, vr1c4, vr1c5, vr1c6, vr1c7, vr1c8, vr1c9 = [StringVar() for i in range(9)]
+vr2c1, vr2c2, vr2c3, vr2c4, vr2c5, vr2c6, vr2c7, vr2c8, vr2c9 = [StringVar() for i in range(9)]
+vr3c1, vr3c2, vr3c3, vr3c4, vr3c5, vr3c6, vr3c7, vr3c8, vr3c9 = [StringVar() for i in range(9)]
+vr4c1, vr4c2, vr4c3, vr4c4, vr4c5, vr4c6, vr4c7, vr4c8, vr4c9 = [StringVar() for i in range(9)]  
+vr5c1, vr5c2, vr5c3, vr5c4, vr5c5, vr5c6, vr5c7, vr5c8, vr5c9 = [StringVar() for i in range(9)]
+vr6c1, vr6c2, vr6c3, vr6c4, vr6c5, vr6c6, vr6c7, vr6c8, vr6c9 = [StringVar() for i in range(9)]
+vr7c1, vr7c2, vr7c3, vr7c4, vr7c5, vr7c6, vr7c7, vr7c8, vr7c9 = [StringVar() for i in range(9)]
+vr8c1, vr8c2, vr8c3, vr8c4, vr8c5, vr8c6, vr8c7, vr8c8, vr8c9 = [StringVar() for i in range(9)]
+vr9c1, vr9c2, vr9c3, vr9c4, vr9c5, vr9c6, vr9c7, vr9c8, vr9c9 = [StringVar() for i in range(9)]
 
 # Label
 for i in range(9):  
     Label(frm1, text=str(i+1)).grid(row=0,column=i+1)
     Label(frm1, text=str(i+1)).grid(row=i+1,column=0)
-# Row 1
-r1c1 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=1,column=1)
-r1c2 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=1,column=2)
-r1c3 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=1,column=3)
-r1c4 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=1,column=4)
-r1c5 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=1,column=5)
-r1c6 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=1,column=6)
-r1c7 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=1,column=7)
-r1c8 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=1,column=8)
-r1c9 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=1,column=9)
-# Row 2
-r2c1 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=2,column=1)
-r2c2 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=2,column=2)
-r2c3 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=2,column=3)
-r2c4 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=2,column=4)
-r2c5 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=2,column=5)
-r2c6 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=2,column=6)
-r2c7 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=2,column=7)
-r2c8 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=2,column=8)
-r2c9 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=2,column=9)
-# Row 3
-r3c1 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=3,column=1)
-r3c2 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=3,column=2)
-r3c3 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=3,column=3)
-r3c4 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=3,column=4)
-r3c5 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=3,column=5)
-r3c6 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=3,column=6)
-r3c7 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=3,column=7)
-r3c8 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=3,column=8)
-r3c9 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=3,column=9)
-# Row 4
-r4c1 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=4,column=1)
-r4c2 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=4,column=2)
-r4c3 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=4,column=3)
-r4c4 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=4,column=4)
-r4c5 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=4,column=5)
-r4c6 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=4,column=6)
-r4c7 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=4,column=7)
-r4c8 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=4,column=8)
-r4c9 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=4,column=9)
-# Row 5
-r5c1 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=5,column=1)
-r5c2 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=5,column=2)
-r5c3 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=5,column=3)
-r5c4 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=5,column=4)
-r5c5 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=5,column=5)
-r5c6 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=5,column=6)
-r5c7 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=5,column=7)
-r5c8 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=5,column=8)
-r5c9 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=5,column=9)
-# Row 6
-r6c1 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=6,column=1)
-r6c2 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=6,column=2)
-r6c3 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=6,column=3)
-r6c4 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=6,column=4)
-r6c5 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=6,column=5)
-r6c6 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=6,column=6)
-r6c7 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=6,column=7)
-r6c8 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=6,column=8)
-r6c9 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=6,column=9)
-# Row 7
-r7c1 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=7,column=1)
-r7c2 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=7,column=2)
-r7c3 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=7,column=3)
-r7c4 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=7,column=4)
-r7c5 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=7,column=5)
-r7c6 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=7,column=6)
-r7c7 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=7,column=7)
-r7c8 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=7,column=8)
-r7c9 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=7,column=9)
-# Row 8
-r8c1 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=8,column=1)
-r8c2 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=8,column=2)
-r8c3 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=8,column=3)
-r8c4 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=8,column=4)
-r8c5 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=8,column=5)
-r8c6 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=8,column=6)
-r8c7 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=8,column=7)
-r8c8 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=8,column=8)
-r8c9 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=8,column=9)
-# Row 9
-r9c1 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=9,column=1)
-r9c2 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=9,column=2)
-r9c3 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=9,column=3)
-r9c4 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=9,column=4)
-r9c5 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=9,column=5)
-r9c6 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=9,column=6)
-r9c7 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=9,column=7)
-r9c8 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=9,column=8)
-r9c9 =  Entry(frm1, font=24, width=2, justify=CENTER).grid(row=9,column=9)
 
-frm1.pack()
-Button(root, text="Submit", font=32).pack(pady=10)
-    
+# Row 1
+r1c1 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr1c1).grid(row=1,column=1)
+r1c2 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr1c2).grid(row=1,column=2)
+r1c3 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr1c3).grid(row=1,column=3)
+r1c4 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr1c4).grid(row=1,column=4)
+r1c5 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr1c5).grid(row=1,column=5)
+r1c6 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr1c6).grid(row=1,column=6)
+r1c7 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr1c7).grid(row=1,column=7)
+r1c8 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr1c8).grid(row=1,column=8)
+r1c9 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr1c9).grid(row=1,column=9)
+# Row 2
+r2c1 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr2c1).grid(row=2,column=1)
+r2c2 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr2c2).grid(row=2,column=2)
+r2c3 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr2c3).grid(row=2,column=3)
+r2c4 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr2c4).grid(row=2,column=4)
+r2c5 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr2c5).grid(row=2,column=5)
+r2c6 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr2c6).grid(row=2,column=6)
+r2c7 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr2c7).grid(row=2,column=7)
+r2c8 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr2c8).grid(row=2,column=8)
+r2c9 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr2c9).grid(row=2,column=9)
+# Row 3
+r3c1 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr3c1).grid(row=3,column=1)
+r3c2 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr3c2).grid(row=3,column=2)
+r3c3 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr3c3).grid(row=3,column=3)
+r3c4 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr3c4).grid(row=3,column=4)
+r3c5 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr3c5).grid(row=3,column=5)
+r3c6 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr3c6).grid(row=3,column=6)
+r3c7 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr3c7).grid(row=3,column=7)
+r3c8 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr3c8).grid(row=3,column=8)
+r3c9 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr3c9).grid(row=3,column=9)
+# Row 4
+r4c1 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr4c1).grid(row=4,column=1)
+r4c2 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr4c2).grid(row=4,column=2)
+r4c3 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr4c3).grid(row=4,column=3)
+r4c4 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr4c4).grid(row=4,column=4)
+r4c5 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr4c5).grid(row=4,column=5)
+r4c6 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr4c6).grid(row=4,column=6)
+r4c7 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr4c7).grid(row=4,column=7)
+r4c8 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr4c8).grid(row=4,column=8)
+r4c9 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr4c9).grid(row=4,column=9)
+# Row 5
+r5c1 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr5c1).grid(row=5,column=1)
+r5c2 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr5c2).grid(row=5,column=2)
+r5c3 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr5c3).grid(row=5,column=3)
+r5c4 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr5c4).grid(row=5,column=4)
+r5c5 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr5c5).grid(row=5,column=5)
+r5c6 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr5c6).grid(row=5,column=6)
+r5c7 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr5c7).grid(row=5,column=7)
+r5c8 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr5c8).grid(row=5,column=8)
+r5c9 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr5c9).grid(row=5,column=9)
+# Row 6
+r6c1 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr6c1).grid(row=6,column=1)
+r6c2 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr6c2).grid(row=6,column=2)
+r6c3 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr6c3).grid(row=6,column=3)
+r6c4 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr6c4).grid(row=6,column=4)
+r6c5 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr6c5).grid(row=6,column=5)
+r6c6 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr6c6).grid(row=6,column=6)
+r6c7 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr6c7).grid(row=6,column=7)
+r6c8 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr6c8).grid(row=6,column=8)
+r6c9 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr6c9).grid(row=6,column=9)
+# Row 7
+r7c1 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr7c1).grid(row=7,column=1)
+r7c2 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr7c2).grid(row=7,column=2)
+r7c3 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr7c3).grid(row=7,column=3)
+r7c4 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr7c4).grid(row=7,column=4)
+r7c5 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr7c5).grid(row=7,column=5)
+r7c6 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr7c6).grid(row=7,column=6)
+r7c7 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr7c7).grid(row=7,column=7)
+r7c8 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr7c8).grid(row=7,column=8)
+r7c9 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr7c9).grid(row=7,column=9)
+# Row 8
+r8c1 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr8c1).grid(row=8,column=1)
+r8c2 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr8c2).grid(row=8,column=2)
+r8c3 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr8c3).grid(row=8,column=3)
+r8c4 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr8c4).grid(row=8,column=4)
+r8c5 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr8c5).grid(row=8,column=5)
+r8c6 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr8c6).grid(row=8,column=6)
+r8c7 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr8c7).grid(row=8,column=7)
+r8c8 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr8c8).grid(row=8,column=8)
+r8c9 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr8c9).grid(row=8,column=9)
+# Row 9
+r9c1 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr9c1).grid(row=9,column=1)
+r9c2 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr9c2).grid(row=9,column=2)
+r9c3 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr9c3).grid(row=9,column=3)
+r9c4 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr9c4).grid(row=9,column=4)
+r9c5 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr9c5).grid(row=9,column=5)
+r9c6 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr9c6).grid(row=9,column=6)
+r9c7 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr9c7).grid(row=9,column=7)
+r9c8 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr9c8).grid(row=9,column=8)
+r9c9 =  Entry(frm1, font=24, width=2, justify=CENTER, textvariable=vr9c9).grid(row=9,column=9)
+
+
 def checker():
-    return 0
-# def disableEntry( rc):
-#     rc.configure(state="disabled")
+    print(vr1c1.get())
+    
+frm1.pack()
+Button(root, text="Submit", font=32, command=checker).pack(pady=10)
+    
     
 
 root.mainloop()
